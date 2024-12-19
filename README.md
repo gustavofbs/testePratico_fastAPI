9bddea3088b2438c8c84fbd7dacf150787b1aa7a
gustavofbs <guguboasorte@gmail.com>
2024-12-19 10:40:19 -0300

    adiciona as rotas

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # testePratico_fastAPI
+
+## Requisitos para executar o teste prático:
+
+Poetry | Versão utilizada: 1.8.5
+Python | Versão utilizada: 3.12.8
+
+
+## Como executar
+
+Entre na pasta do projeto:
+
+```bash
+cd toDo_list
+```
+Inicie o servidor
+
+```bash
+fastapi dev todo_list/app.py
+```
+
+## Dependências Instaladas
+
+### FastAPI
+
+```bash
+poetry add 'fastapi[standard]'
+```
+
--- toDo_list/todo_list/app.py
+++ toDo_list/todo_list/app.py
@@ -1,8 +1,50 @@
-from fastapi import FastAPI
+from typing import Optional
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
+tasks = []
+task_id_counter = 1
 
-@app.get('/')
-def read_root():
-    return {'message': 'Olá Mundo!'}
+class Task(BaseModel):
+    title: str
+    description: Optional[str] = None
+
+
+class UpdateTaskStatus(BaseModel):
+    status: str
+
+
+@app.post('/tasks')
+def create_task(task: Task):
+    global task_id_counter
+    new_task = {
+        "id": task_id_counter,
+        "title": task.title,
+        "description": task.description,
+        "status": "pending"
+    }
+
+    tasks.append(new_task)
+    task_id_counter += 1
+    return new_task
+
+@app.get('/tasks')
+def list_tasks():
+    return tasks
+
+@app.put('/tasks/{task_id}')
+def update_task(task_id: int, update: UpdateTaskStatus):
+    for task in tasks:
+        if task['id'] == task_id:
+            task['status'] = update.status
+            return task
+    raise HTTPException(status_code=404, detail='Task Not Found')
+
+@app.delete('/tasks/{task_id}')
+def delete_task(task_id: int):
+    global tasks
+    tasks = [task for task in tasks if task['id'] != task_id]
+    return {"message": "Task deleted sucessfuly"}
